b3181fcac1d43f63cebd63a0624116d0a6366a05
Sanjay V Jacob <76661259+sanjayvjacob@users.noreply.github.com>
2023-12-23 23:47:18 +0000

    Add if else statement, also fix bug

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 prompt("What is your name?");
 prompt("what is your lover's name?");
 var loveScore = Math.random() * 100;
-loveScore = Math.floor() +1;
+loveScore = Math.floor(loveScore) +1;
 console.log(loveScore);
-alert("Your love score is " + loveScore + "%");
+if(loveScore > 70){
+  alert("Your love score is " + loveScore + "%" + "You love each other like kanye loves kanye");
+}
+else{
+  alert("Your love score is " + loveScore + "%");
+}
