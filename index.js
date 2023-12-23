f14e103754336a636cbc94bcf06df5b1ebc1297b
Sanjay V Jacob <76661259+sanjayvjacob@users.noreply.github.com>
2023-12-23 23:30:11 +0000

    Random Number Generation

---
+++ index.js
@@ -0,0 +1,6 @@
+prompt("What is your name?");
+prompt("what is your lover's name?");
+var loveScore = Math.random() * 100;
+loveScore = Math.floor() +1;
+console.log(loveScore);
+alert("Your love score is " + loveScore + "%");
